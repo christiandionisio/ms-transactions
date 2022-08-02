b84d2e1f2579c44a441f451cfbf0554ea16c88bc
Alisson <arteaga10alisson@gmail.com>
2022-08-02 18:14:51 -0500

    update

--- src/main/java/com/example/mstransactions/data/dto/TransactionCommissionDto.java
+++ src/main/java/com/example/mstransactions/data/dto/TransactionCommissionDto.java
@@ -11,5 +11,5 @@ public class TransactionCommissionDto {
     private String transactionType;
     private LocalDateTime transactionDate;
     private String productType;
-    private String commissionAmount;
+    private BigDecimal commissionAmount;
 }
