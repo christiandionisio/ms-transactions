b24774b105deb64885e7dbe3e66ebd331e2aa4c6
Christian Dionisio <christiandionisio9432@gmail.com>
2022-08-16 16:59:36 -0500

    refactor functions

--- src/main/java/com/example/mstransactions/kafka/consumer/WalletPaymentConsumer.java
+++ src/main/java/com/example/mstransactions/kafka/consumer/WalletPaymentConsumer.java
@@ -3,6 +3,7 @@ package com.example.mstransactions.kafka.consumer;
 import com.example.mstransactions.data.Account;
 import com.example.mstransactions.data.dto.PaymentDto;
 import com.example.mstransactions.data.dto.TransactionDto;
+import com.example.mstransactions.model.Transaction;
 import com.example.mstransactions.service.TransactionService;
 import com.example.mstransactions.utils.TransactionUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
@@ -27,18 +29,10 @@ public class WalletPaymentConsumer {
         TransactionDto transactionDto = new TransactionDto();
 
         transactionUtil.findAccountByWalletPhoneNumber(paymentDto.getPhoneNumberOrigin())
-                .flatMap(accountOrigin -> {
-                    transactionDto.setOriginAccount(accountOrigin.getAccountId());
-                    return transactionUtil.findAccountByWalletPhoneNumber(paymentDto.getPhoneNumberDestination());
-                })
-                .flatMap(accountDestination -> {
-                    setTransactionData(transactionDto, paymentDto, accountDestination);
-                    return transactionService.transferBetweenAccounts(transactionDto);
-                })
-                .doOnNext(transaction -> log.info("Transaction created {} ", transaction))
+                .flatMap(accountOrigin -> findAccountDestination(transactionDto, accountOrigin, paymentDto))
+                .flatMap(accountDestination -> makeTransaction(transactionDto, paymentDto, accountDestination))
+                .doOnSuccess(this::setLoggerResponse)
                 .subscribe();
-
-
     }
 
     private void setTransactionData(TransactionDto transactionDto, PaymentDto paymentDto,
@@ -48,6 +42,32 @@ public class WalletPaymentConsumer {
         transactionDto.setProductId(transactionDto.getOriginAccount());
         transactionDto.setCommerceName("YANKI WALLET");
         transactionDto.setTransactionDate(paymentDto.getDateTime());
+    }
+
+    private void setLoggerResponse(Transaction transaction) {
+        if (transaction != null) {
+            log.info("Transaction created {} ", transaction);
+        } else {
+            log.info("Transaction not created");
+        }
+    }
+
+    private Mono<Transaction> makeTransaction(TransactionDto transactionDto, PaymentDto paymentDto,
+                                            Account accountDestination) {
+        setTransactionData(transactionDto, paymentDto, accountDestination);
+        if (accountDestination.getAccountId() == null) {
+            log.info("Account destination not found");
+            log.info("Only make withdrawal");
+            return transactionService.makeWithdrawal(transactionDto);
+        }
+        return transactionService.transferBetweenAccounts(transactionDto);
+    }
+
+    private Mono<Account> findAccountDestination(TransactionDto transactionDto, Account accountOrigin,
+                                                 PaymentDto paymentDto) {
+        transactionDto.setOriginAccount(accountOrigin.getAccountId());
+        return transactionUtil.findAccountByWalletPhoneNumber(paymentDto.getPhoneNumberDestination())
+                .defaultIfEmpty(new Account());
     }
 
 }
